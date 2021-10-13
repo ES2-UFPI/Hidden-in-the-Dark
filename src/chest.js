30db0c5663ae4d10655d519b4dd7a526ca53dec4
Gustavo André <tiratreco@gmail.com>
2021-10-13 16:35:05 -0300

    inserção do baú no mapa #6

---
+++ src/chest.js
@@ -0,0 +1,48 @@
+
+
+export default class Chest {
+    
+    constructor(id){
+        this.id = id;
+        this.caminho = '/src/assets/items/chest.png';
+        this.name = 'chest-'+this.id;
+    }
+
+    preload(preload) {
+        preload.load.spritesheet(this.name, this.caminho, {
+            frameWidth: 54,
+            frameHeight: 54,
+        });
+    }
+
+    create (create, coord){
+
+        this.chest = create.physics.add.staticSprite(coord['x'], coord['y'], this.name);
+        
+        const anims = create.anims;
+
+        anims.create({
+            key: "close",
+            frames: anims.generateFrameNames(this.name, { start: 77, end: 77 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+        anims.create({
+            key: "open",
+            frames: anims.generateFrameNames(this.name, { start: 77, end: 81 }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+
+        this.chest.anims.play("close", true);
+    }
+
+    open (){
+
+
+
+        //dar chave
+    }
+
+}
