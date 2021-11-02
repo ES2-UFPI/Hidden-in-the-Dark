f364bbd419874aaf860d2c84b7a9d73f4782767a
Felipe Torres <felipetoress@gmail.com>
2021-11-02 13:43:47 -0300

    implementando CI e CD

--- src/hidder.js
+++ src/hidder.js
@@ -4,27 +4,27 @@ export default class Hidder extends Player {
 
     constructor(game, id, velocidade){
         super(game, id, velocidade);
-        // this.lastChest = null;
+        this.lastChest = null;
     }
 
-    // interactions(chests){
-    //     super.interactions(chests);
+    interactions(chests){
+        super.interactions(chests);
         
-    //     chests.forEach((c) => {//interações com baús
-    //         this.game.physics.add.overlap(this.player, c.zone, ()=>{
-    //             this.lastChest = c;
-    //         });
-    //     });
+        chests.forEach((c) => {//interações com baús
+            this.game.physics.add.overlap(this.player, c.zone, ()=>{
+                this.lastChest = c;
+            });
+        });
         
-    // }
+    }
 
     update(button){
 
-        // if(button.space.isDown){
-        //     if (this.lastChest != null && this.game.physics.overlap(this.player, this.lastChest.zone)) {
-        //         this.lastChest.open();
-        //     }
-        // }
+        if(button.space.isDown){
+            if (this.lastChest != null && this.game.physics.overlap(this.player, this.lastChest.zone)) {
+                this.lastChest.open();
+            }
+        }
         super.update(button);
         
     }
